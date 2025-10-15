b38d2a9a9c8a765225cf8129b4998e2a24c81859
Olivia Barker <olivia.barker@centre.edu>
2025-10-15 13:44:36 -0400

    Paint

--- src/main/java/PaintBrush.java
+++ src/main/java/PaintBrush.java
@@ -70,8 +70,110 @@ set the "paint" for the paintbrush
 		{
 			mesh[x][y]=this.getPaint();
 		}
+		else if (mode==BrushMode.fillMode)
+		{
+			fill(x,y, mesh, mesh[x][y]);
+		}
+		else if (mode==BrushMode.pattern1Mode)
+		{
+			boolean bool = true;
+			pattern1(x,y, mesh, mesh[x][y], bool);
+		}
+		else if (mode==BrushMode.pattern2Mode)
+		{
+			pattern2(x,y, mesh, mesh[x][y]);
+		}
 	}
+	
+	
+	public void pattern2(int x, int y, Paint[][] mesh, Paint current)
+	{
+		Stack<Integer> stackx = new Stack<Integer>();
+		Stack<Integer> stacky = new Stack<Integer>();
+		stackx.push(x);
+		stacky.push(y);
+		while (!stackx.isEmpty())
+		{
+			int tempx = stackx.pop();
+			int tempy = stacky.pop();
+			if (tempy%2==0) 
+			{
+				mesh[tempx][tempy]=this.paint;
+			}
+			else
+			{
+				mesh[tempx][tempy]=this.White;
+			}
+			if (mesh[tempx+1][tempy].equals(current))
+			{
+				stackx.push(tempx+1);
+				stacky.push(tempy);
+			}
+			else if (mesh[tempx-1][tempy].equals(current))
+			{
+				stackx.push(tempx-1);
+				stacky.push(tempy);
+			}
+			else if (mesh[tempx][tempy+1].equals(current))
+			{
+				stackx.push(tempx);
+				stacky.push(tempy+1);
+			}
+			else if (mesh[tempx][tempy-1].equals(current))
+			{
+				stackx.push(tempx);
+				stacky.push(tempy-1);
+			}
+		}		
+	}
+	
 
+	
+	
+	public void pattern1(int x, int y, Paint[][] mesh, Paint current, boolean bool)
+	{
+		if (!mesh[x][y].equals(current) || mesh[x][y].equals(this.paint))
+		{
+			return;
+		}
+		else
+		{
+			if (bool)
+			{ 
+				mesh[x][y]=this.paint;
+			}
+			pattern1(x+1,y, mesh, current, !bool);			
+			pattern1(x-1,y, mesh, current, !bool);			
+			pattern1(x,y+1, mesh, current, !bool);			
+			pattern1(x,y-1, mesh, current, !bool);			
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//make sure check for in domain later	
+	public void fill(int x, int y, Paint[][] mesh, Paint current)
+	{
+		if (!mesh[x][y].equals(current) || mesh[x][y].equals(this.paint))
+		{
+			return;
+		}
+		else
+		{
+			mesh[x][y]=this.paint;
+			fill(x+1,y,mesh, current);
+			fill(x-1,y,mesh, current);
+			fill(x,y+1,mesh, current);
+			fill(x,y-1,mesh, current);
+		}
+	}
 	
 	
 /*
